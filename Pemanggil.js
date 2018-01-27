06e515aed6d0598e01aa925daf7a7b212271339a
Azizbuhori <35867515+Azizbuhori@users.noreply.github.com>
2018-01-28 05:23:18 +0800

    Update Pemanggil.js

--- Pemanggil.js
+++ Pemanggil.js
@@ -22,11 +22,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2017/08/car-production-fall-hits-uk-industrial.html"}
-if(x=="2"){xxx="/2017/08/tesla-car-battery-production-releases.html"}
-if(x=="3"){xxx="/2017/08/green-lunacy-fossil-fuel-mandates-to.html"}
-if(x=="4"){xxx="/2017/08/us-climate-report-edits-out-highly_28.html"}
-if(x=="5"){xxx="/2017/08/arctic-melt-season-changes-and-arctic.html"}a_to_ve[i].href="http://www.stekai.ml"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="post1"){xxx="http://www.stekai.ml/2018/01/e-invoicing-in-7-steps.html"}
+if(x=="post2"){xxx="http://www.stekai.ml/2018/01/4-instant-hacks-to-stop-burning-as-much.html"}
+if(x=="post3"){xxx="http://www.stekai.ml/2018/01/financial-skills-opening-bank-account_27.html"}
+if(x=="post4"){xxx="http://www.stekai.ml/2018/01/credit-card-secrets-3-great-tips-for.html"}
+if(x=="post5"){xxx="http://www.stekai.ml/2018/01/business-tax-strategies-what-works-and.html"}a_to_ve[i].href="http://www.stekai.ml"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
